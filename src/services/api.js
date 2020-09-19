e1c7e19d1b94c7c8e3da7a01c3a9e43eaedd443c
alanrbentes <alanrbentes@gmail.com>
2020-09-19 18:05:46 -0300

    Ajuste ip

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://192.168.1.72:3333',
-  baseURL: 'https://67.205.142.63'
+  baseURL: 'http://xxx.xxx.xx.x:yyyy',
 });
 
 export default api;
